2a17634302ffbfe582e628cb282343c49e5f104d
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-25 17:16:33 +0200

    Blog

--- client/blog/src/redux/reducers/postReducer.js
+++ client/blog/src/redux/reducers/postReducer.js
@@ -1,82 +1,103 @@
-import * as api from '../api';
+import * as types from '../actions/types';
 
-const FETCH_POSTS_REQUEST = 'FETCH_POSTS_LOADING';
-const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
-const FETCH_POSTS_FAIL = 'FETCH_POSTS_FAIL';
+const initialState = {
+  posts: [],
+  currentPost: null,
+};
 
-const FETCH_SINGLE_POST_REQUEST = 'FETCH_SINGLE_POST_REQUEST';
-const FETCH_SINGLE_POST_SUCCESS = 'FETCH_SINGLE_POST_SUCCESS';
-const FETCH_SINGLE_POST_FAIL = 'FETCH_SINGLE_POST_FAIL';
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.FETCH_POSTS_REQUEST:
+      return {
+        loading: true,
+        posts: [],
+      };
+    case types.FETCH_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false,
+      };
+    case types.FETCH_POSTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
 
-const CREATE_POST_REQUEST = 'CREATE_POST_REQUEST';
-const CREATE_POST_SUCCESS = 'CREATE_POST_SUCCESS';
-const CREATE_POST_FAIL = 'CREATE_POST_FAIL';
+    case types.FETCH_SINGLE_POST_REQUEST:
+      return {
+        loading: true,
+        posts: [],
+      };
+    case types.FETCH_SINGLE_POST_SUCCESS:
+      return {
+        ...state,
+        currentPost: action.payload,
+        loading: false,
+      };
+    case types.FETCH_SINGLE_POST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
 
-const UPDATE_POST_REQUEST = 'UPDATE_POST_REQUEST';
-const UPDATE_POST_SUCCESS = 'UPDATE_POST_SUCCESS';
-const UPDATE_POST_FAIL = 'UPDATE_POST_FAIL';
+    case types.CREATE_POST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.CREATE_POST_SUCCESS:
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+        loading: false,
+      };
+    case types.CREATE_POST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
 
-const DELETE_POST_REQUEST = 'DELETE_POST_REQUEST';
-const DELETE_POST_SUCCESS = 'DELETE_POST_SUCCESS';
-const DELETE_POST_FAIL = 'DELETE_POST_FAIL';
+    case types.DELETE_POST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.DELETE_POST_SUCCESS:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+        currentPost: null,
+        loading: false,
+      };
+    case types.DELETE_POST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case types.UPDATE_POST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.UPDATE_POST_SUCCESS:
+      return {
+        ...state,
+        posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)),
+        currentPost: action.payload,
+        loading: false,
+      };
+    case types.UPDATE_POST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
 
-export const fetchPosts = () => async (dispatch) => {
-  try {
-    dispatch({ type: FETCH_POSTS_REQUEST });
-    const { data } = await api.fetchPosts();
-    dispatch({ type: FETCH_POSTS_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: FETCH_POSTS_FAIL, payload: error.message });
+    default:
+      return {
+        ...state,
+      };
   }
 };
 
-export const fetchSinglePost = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: FETCH_SINGLE_POST_REQUEST });
-    const { data } = await api.fetchSinglePost(id);
-    dispatch({ type: FETCH_SINGLE_POST_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: FETCH_SINGLE_POST_FAIL, payload: error.message });
-  }
-};
-
-export const createPost = (post) => async (dispatch) => {
-  try {
-    dispatch({ type: CREATE_POST_REQUEST, payload: post });
-    const { data } = await api.createPost(post);
-    dispatch({
-      type: CREATE_POST_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({ type: CREATE_POST_FAIL, payload: error.message });
-  }
-};
-
-export const updatePost = (id, post) => async (dispatch) => {
-  try {
-    dispatch({ type: UPDATE_POST_REQUEST });
-    const { data } = await api.updatePost(id, post);
-    dispatch({
-      type: UPDATE_POST_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({ type: UPDATE_POST_FAIL, payload: error.message });
-  }
-};
-
-export const deletePost = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: DELETE_POST_REQUEST });
-    const { data } = await api.deletePost(id);
-    dispatch({
-      type: DELETE_POST_SUCCESS,
-      payload: data._id,
-    });
-  } catch (error) {
-    dispatch({ type: DELETE_POST_FAIL, payload: error.message });
-  }
-};
-
-export const postReducer = { deletePost, fetchPosts, fetchSinglePost, createPost, updatePost };
+export { postReducer };
