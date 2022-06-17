3a135fb3fa2f7806aaab04d32627847ff7c1ecfd
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-17 14:23:26 +0200

    fetch token dispatch uid

--- client/blog/src/App.js
+++ client/blog/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { UidContext } from './AppContext';
+import axios from 'axios';
 import FormPost from './components/post/FormPost';
 import GetPost from './components/post/GetPost';
 
 const App = () => {
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => {
+          setUid(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchToken();
+  }, []);
+
   return (
-    <div>
-      <FormPost />
-      <GetPost />
-    </div>
+    <UidContext.Provider value={uid}>
+      <div>
+        <FormPost />
+        <GetPost />
+      </div>
+    </UidContext.Provider>
   );
 };
 
