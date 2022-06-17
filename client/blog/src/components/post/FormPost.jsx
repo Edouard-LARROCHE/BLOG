68d89bee3092b30051158f93b4aacfd406de6ea6
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-17 13:28:18 +0200

    initial project

--- client/blog/src/components/post/FormPost.jsx
+++ client/blog/src/components/post/FormPost.jsx
@@ -1,42 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../redux/actions/postAction';
+import React from 'react';
 
 const FormPost = () => {
-  const [title, setTitle] = useState('');
-  const [subtitle, setSubtitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [content, setContent] = useState('');
-  const data = { title, subtitle, author, content };
-
-  const dispatch = useDispatch();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      dispatch(createPost({ ...data }));
-      setTitle('');
-      setSubtitle('');
-      setAuthor('');
-      setContent('');
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <div>
       <h1>Form Post</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='title' name='title' onChange={(e) => setTitle(e.target.value)} value={title} required />
-          <input type='text' placeholder='subtitle' name='subtitle' onChange={(e) => setSubtitle(e.target.value)} value={subtitle} required />
-          <input type='text' placeholder='author' name='author' onChange={(e) => setAuthor(e.target.value)} value={author} required />
-          <input type='text' placeholder='content' name='content' onChange={(e) => setContent(e.target.value)} value={content} required />
-          <button type='submit'>Poster</button>
-        </form>
-      </div>
     </div>
   );
 };
--- client/blog/src/components/post/GetPost.jsx
+++ client/blog/src/components/post/GetPost.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts } from '../../redux/actions/postAction';
-import Post from './Post';
+import React from 'react';
 
 const GetPost = () => {
-  const postsData = useSelector((state) => state.posts.posts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchPosts());
-  }, [dispatch]);
-
   return (
     <div>
       <h1>POSTS</h1>
-      {postsData.map((index) => (
-        <Post key={index._id} title={index.title} subtitle={index.subtitle} author={index.author} content={index.content} />
-      ))}
     </div>
   );
 };
