4e32b866f16488402fd12466bf3e921a9a4044d1
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-17 16:16:28 +0200

    component Login

--- client/blog/src/App.js
+++ client/blog/src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { UidContext } from './AppContext';
 import axios from 'axios';
-// REDUX
-import FormPost from './components/Post/FormPost';
+import Login from './components/Log/Login';
 import GetPost from './components/Post/GetPost';
+// REDUX
 import { useDispatch } from 'react-redux';
 import { getUserData } from './feature-redux/user.slice';
 
@@ -15,7 +15,7 @@ const App = () => {
     const fetchToken = async () => {
       await axios({
         method: 'get',
-        url: `${process.env.REACT_APP_API}jwtid`,
+        url: `${process.env.REACT_APP_API}/jwtid`,
         withCredentials: true,
       })
         .then((res) => {
@@ -25,13 +25,15 @@ const App = () => {
     };
     fetchToken();
 
+    console.log(uid);
+
     if (uid) dispatch(getUserData(uid));
   }, [dispatch, uid]);
 
   return (
     <UidContext.Provider value={uid}>
       <div>
-        <FormPost />
+        <Login />
         <GetPost />
       </div>
     </UidContext.Provider>
--- client/blog/src/components/Log/Login.jsx
+++ client/blog/src/components/Log/Login.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-  return <div>login</div>;
+  const [data, setData] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:5500/user/login', data)
+      .then((res) => {
+        if (res.data.errors) {
+          setError(res.data.errors.password || res.data.errors.email);
+        } else {
+          window.location = '/';
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>SE CONNECTER</h1>
+        <input type='email' placeholder='Adresse mail' name='email' onChange={handleChange} value={data.email} required />
+        <input type='password' placeholder='Mot de passe' name='password' onChange={handleChange} value={data.password} required />
+        {error && <p>{error}</p>}
+        <button type='submit'>
+          <p>CONNEXION</p>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
