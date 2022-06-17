62f0648f089bf6eddadb56b7859b0add00d53cbc
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-17 14:03:13 +0200

    get posts redux

--- client/blog/src/components/post/GetPost.jsx
+++ client/blog/src/components/post/GetPost.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setPostsData } from '../../feature-redux/posts.slice';
 
 const GetPost = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get('http://localhost:5500/posts').then((res) => dispatch(setPostsData(res.data)));
+  }, [dispatch]);
+
   return (
     <div>
       <h1>POSTS</h1>
