91ee89ba79b101dd3dbb855ec352e37251be6692
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-20 16:13:51 +0200

    page log

--- client/blog/src/pages/Home.jsx
+++ client/blog/src/pages/Home.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Register from '../components/Log/Register';
-import Login from '../components/Log/Login';
 import GetPost from '../components/Post/GetPost';
 
 const Home = () => {
   return (
     <div className='home-blog'>
-      <Register />
-      <Login />
-
       <GetPost />
     </div>
   );
--- client/blog/src/routes/Routes.js
+++ client/blog/src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../pages/Home';
+import Log from '../pages/Log';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <NavBar />
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route path='/log' element={<Log />} />
       </Routes>
     </div>
   );
