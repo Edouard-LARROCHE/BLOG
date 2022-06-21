e24fbfda446ec3c19fbf4fd336889c664aa72f7a
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-21 12:22:44 +0200

    style popup

--- client/blog/src/components/NavBar.jsx
+++ client/blog/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react';
-import { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { UidContext } from '../AppContext';
@@ -8,21 +7,17 @@ import Logout from './Log/Logout';
 const NavBar = () => {
   const uid = useContext(UidContext);
   const userConnectData = useSelector((state) => state.user);
-  const [popupLog, setPopupLog] = useState('log-popup');
+  const [popupLog, setPopupLog] = useState('popup');
 
   return (
     <div className='navBar'>
       {/* <img src='./assets/ICONS/logo.png' alt='logo-blog' style={{ width: '80px' }} /> */}
       <h2>Stack-BLOG</h2>
       {uid ? (
-        <ul>
-          <li>
-            <h5>Bienvenue {userConnectData.pseudo}</h5>
-          </li>
-          <li>
-            <Logout />
-          </li>
-        </ul>
+        <div className='user-connect'>
+          <h5>Bienvenue {userConnectData.pseudo}</h5>
+          <Logout />
+        </div>
       ) : (
         <div className={popupLog}>
           <Link to='/log'>
@@ -30,8 +25,8 @@ const NavBar = () => {
               src='./assets/SVG/login.svg'
               alt='login'
               style={{ width: '30px', marginTop: '20px' }}
-              onMouseOver={() => setPopupLog('log-popup-after')}
-              onMouseLeave={() => setPopupLog('log-popup')}
+              onMouseOver={() => setPopupLog('popup-after')}
+              onMouseLeave={() => setPopupLog('popup')}
             />
           </Link>
           <p>Se connecter</p>
