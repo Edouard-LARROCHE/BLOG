d4e1cae5eda02dc3272f267d2df3954cc4a21d8f
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-28 17:06:12 +0200

    follow user

--- client/blog/src/components/Users/FollowUser.jsx
+++ client/blog/src/components/Users/FollowUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { followUser } from '../../feature-redux/user.slice';
+import { getUsers } from '../../feature-redux/users.slice';
 
 const FollowUser = ({ followId }) => {
   const userData = useSelector((state) => state.user);
@@ -8,6 +9,7 @@ const FollowUser = ({ followId }) => {
 
   const handleFollow = () => {
     dispatch(followUser(userData._id, followId));
+    dispatch(getUsers(userData._id));
   };
   return (
     <div>
