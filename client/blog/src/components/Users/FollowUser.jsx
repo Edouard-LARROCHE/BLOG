12cedd32620cbd5ef30aaa0f399420b78653ed8c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-28 10:42:48 +0200

    follow user

---
+++ client/blog/src/components/Users/FollowUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FollowUser = ({ users }) => {
+  return (
+    <div>
+      <p>follow</p>
+    </div>
+  );
+};
+
+export default FollowUser;
--- client/blog/src/components/Users/Users.jsx
+++ client/blog/src/components/Users/Users.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FollowUser from './FollowUser';
 
 const Users = ({ users }) => {
   return (
     <div>
       <h1> {users.pseudo} </h1>
       <h2> {users.bio} </h2>
+      <FollowUser users={users} />
     </div>
   );
 };
